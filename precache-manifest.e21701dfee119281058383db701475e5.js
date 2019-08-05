7f75950f157f50220f1f999996d4b70a11a33f71
unknown <mariia>
2019-08-05 21:47:52 +0300

    Updates

---
+++ precache-manifest.e21701dfee119281058383db701475e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98b65dcf48ed38f74078217cc50d5ff",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "53ca66aa8ef7bd289d90",
+    "url": "/react_pagination/static/css/main.a55995b3.chunk.css"
+  },
+  {
+    "revision": "43e72d7b3cbb74a2a37d",
+    "url": "/react_pagination/static/js/2.fb2b5beb.chunk.js"
+  },
+  {
+    "revision": "53ca66aa8ef7bd289d90",
+    "url": "/react_pagination/static/js/main.f02d57ce.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
